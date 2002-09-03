570d2e94b843b1aa49a386e9ba10d274fa1a387e
Enils Bashi <enils@lidar.net>
2002-09-03 22:20:00 +0000

    Gnav is a simple fix editor that will be used to fix Rinex files. Fix button is not working yet.

--- src/gnav.tcl
+++ src/gnav.tcl
@@ -1,27 +1,62 @@
-#!/usr/local/ActiveTcl/bin/wish
-
 #
-#
-#  $Id$
-#
-#  Orginal W. Wright 8/25/2002
+# $Id$
 #
 
-    set ifn [ tk_getOpenFile -initialdir "/data/" ]
-    set ifd [ open $ifn "r" ] 
-    
-    set data [ read $ifd] 
+proc createFix {} {
+scrollbar .ys -command ".t yview"
+text .t -wrap word -yscrollcommand ".ys set"
+pack .t .ys -side left -fill y
+}
 
-    text .txt \
-	-width 125 \
-	-height 20 \
-	-wrap none \
- 	-yscrollcommand { .ysbar set }	
-	scrollbar .ysbar -orient vertical   -command { .txt yview }
-    grid .txt .ysbar -sticky nsew
-    grid columnconfigure . 0 -weight 1
-    .txt insert end $data	
- 
+proc createMain {} {
 
+wm title . "Gnav-Editor"
+menu .menubar
+.menubar add cascade -menu .menubar.file -label "File" -underline 0
+.menubar add cascade -menu .menubar.fix -label "Fix" -underline 0
+menu .menubar.file -tearoff 1
+.menubar.file add command -label Open -command openfile -underline 0
+.menubar.file add command -label Save -command save -underline 0
+.menubar.file add command -label "Save As" -command saveas -underline 0
+.menubar.file add separator
+.menubar.file add command -label Quit -command exit -underline 0
+menu .menubar.fix -tearoff 1
+.menubar.fix add command -label "Fix File" 
+. configure -menu .menubar
+}
 
+proc openfile {} {
+  set ftype {
+ 	{"Text Files" {.txt .TXT .doc .DOC }}
+	{ "All Files" * }
+  }
+  
+ fname tk_getOpenFile 
+  set f [open $::f]
+  .t insert end [read $f]
+  close $f
+}
 
+proc save {} {
+set f [open $::f w]
+puts $f [.t get 1.0 end]
+close $f
+}
+
+proc saveas {} {
+fname tk_getSaveFile
+save
+}
+
+ proc fname proc {
+   set new [$proc]
+   if {{} == $new} {
+   return
+   }
+   set ::f $new
+ }
+
+eval destroy [winfo child .]
+set f {}
+createFix
+createMain
