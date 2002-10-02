3f02cddf8aa516e072028aa458a2cd0167529307
Enils Bashi <enils@lidar.net>
2002-10-02 16:43:37 +0000

    Fixed the FTP function. Added a status bar to show the user  the current status of the ftp session.

--- src/gnav.tcl
+++ src/gnav.tcl
@@ -13,18 +13,22 @@ set plat $tcl_platform(platform)
 
 proc createFix {} {
 global plat
-
-scrollbar .ys -command ".t yview"
-
+frame .f
+pack .f
+frame .f1
+pack .f1 -after .f -side bottom 
+scrollbar .f.ys -command ".f.t yview"
+scrollbar .f1.ys1 -command ".f1.t1 yview"
 if { ($plat) == "windows" } {
 set sys ansifixed
-text .t -font "$sys" \
-	-width 80 -height 30 -wrap none -yscrollcommand ".ys set"
+text .f.t -font "$sys" \
+	-width 80 -height 30 -wrap none -yscrollcommand ".f.ys set"
 } else {
-text .t -width 80 -height 30 -wrap none -yscrollcommand ".ys set"
+text .f.t -width 80 -height 30 -wrap none -yscrollcommand ".f.ys set"
 }
-
-pack .t .ys -side left -fill y
+text .f1.t1 -width 80 -height 4 -wrap none -yscrollcommand ".f1.ys1 set"
+pack .f.t .f.ys -side left -fill y 
+pack .f1.t1 .f1.ys1 -side left -fill y 
 }
 
 proc createMain {} {
@@ -56,13 +60,13 @@ menu .menubar.fix.antena.m
 proc openfile {} {
  fname tk_getOpenFile 
   set f [open $::f] 
-  .t insert end [read $f]
+  .f.t insert end [read $f]
   close $f
 }
 
 proc save {} {
 set f [open $::f w]
-puts $f [.t get 1.0 end]
+puts $f [.f.t get 1.0 end]
 close $f
 }
 
@@ -79,9 +83,30 @@ set li [ PasswdDlg .pw -logintextvariable usr \
 
 set usr [lindex $li 0 ]
 set passwd [lindex $li 1]
-set ftpsession [ ::ftp::Open lidar.net $usr $passwd ] 
+if { [ set ftpsession [ ::ftp::Open lidar.net $usr $passwd -progress xstat ]] == -1 } {    
+   .f1.t1 insert 2.0 "Connection Refused! Please try again... \n"
+} else {
+.f1.t1 delete 0.0 0.end
+.f1.t1 insert 0.0 "Opening FTP session .... \n"
+.f1.t1 insert 1.0 "Logging in .... \n"
+.f1.t1 insert 2.0 "Transfer Started ... Please wait! \n"
+
+set ::ftp::VERBOSE 1 
 #set chdir [ ::ftp::Cd $ftpsession /var/ftp/pub ]
-set send [ ::ftp::Put $ftpsession $::f ]
+::ftp::Put $ftpsession $::f 
+::ftp::Close $ftpsession 
+}
+}
+# xstat procedure prints the percentage transfered via ftp
+proc xstat { b } {
+  set size [ file size $::f ]
+  set pc [ expr $b * 100/$size ]
+  set i 3
+  .f1.t1 insert $i.0 "$b bytes transfered .... $pc % \n"
+  if { $pc == 100 } {
+  .f1.t1 insert $i.0 "Transfered Completed... \n"
+  }
+  incr i 
 }
 
 proc fname proc {
@@ -98,64 +123,64 @@ global j
 set j 11
 
 # insert user name and company 
-.t delete 5.00 5.24 
-.t insert 5.00 "Virg                Nasa"
+.f.t delete 5.00 5.24 
+.f.t insert 5.00 "Virg                Nasa"
 
 # insert blank spaces
-.t delete 10.12 10.60
-.t insert 10.12 "                                                "
+.f.t delete 10.12 10.60
+.f.t insert 10.12 "                                                "
 
 for {set i 11} { $i <= 20} {incr i} {
-set idx [ .t search -forwards -exact "WAVELENGTH" $i.0 $i.end]
+set idx [ .f.t search -forwards -exact "WAVELENGTH" $i.0 $i.end]
 if {[regexp {[1-9]} $idx] == 1} {
-.t delete $i.0 $i.end
+.f.t delete $i.0 $i.end
 incr j
 }
 }
 
 # delete blank lines
-.t delete 11.0 $j.0
+.f.t delete 11.0 $j.0
 }
 
 proc hgr58 {} {
-.t delete 6.20 6.36
-.t insert 6.20 "ASH UZ 12       "
-.t delete 7.20 7.36
-.t insert 7.20 "ASH 700718B     "
+.f.t delete 6.20 6.36
+.f.t insert 6.20 "ASH UZ 12       "
+.f.t delete 7.20 7.36
+.f.t insert 7.20 "ASH 700718B     "
 }
 
 proc niiix {} {
-.t delete 6.20 6.36
-.t insert 6.20 "ASH UZ 12       "
-.t delete 7.20 7.36
-.t insert 7.20 "ASH 700228D     "
+.f.t delete 6.20 6.36
+.f.t insert 6.20 "ASH UZ 12       "
+.f.t delete 7.20 7.36
+.f.t insert 7.20 "ASH 700228D     "
 }
 
 proc usgs1 {} {
-.t delete 6.20 6.36
-.t insert 6.20 "ASH UZ 12       "
-.t delete 7.20 7.36
-.t insert 7.20 "ASH 7009936     "
-.t delete 9.8 9.15
-.t insert 9.8 " 2.0000"
+.f.t delete 6.20 6.36
+.f.t insert 6.20 "ASH UZ 12       "
+.f.t delete 7.20 7.36
+.f.t insert 7.20 "ASH 7009936     "
+.f.t delete 9.8 9.15
+.f.t insert 9.8 " 2.0000"
 }
 
 proc usgs2 {} {
-.t delete 6.20 6.36
-.t insert 6.20 "ASH UZ 12       "
-.t delete 7.20 7.36
-.t insert 7.20 "ASH 7009936     "
-.t delete 9.8  9.15
-.t insert 9.8 " 1.2400"
+.f.t delete 6.20 6.36
+.f.t insert 6.20 "ASH UZ 12       "
+.f.t delete 7.20 7.36
+.f.t insert 7.20 "ASH 7009936     "
+.f.t delete 9.8  9.15
+.f.t insert 9.8 " 1.2400"
 }
 
 proc usgs3 {} { 
-.t delete 6.20 6.36
-.t insert 6.20 "TRM 4700        "
-.t delete 7.20 7.36
-.t insert 7.20 "TRM 33429.00 -GP"
-.t delete 9.8  9.15
-.t insert 9.8 " 1.4600"
+.f.t delete 6.20 6.36
+.f.t insert 6.20 "TRM 4700        "
+.f.t delete 7.20 7.36
+.f.t insert 7.20 "TRM 33429.00 -GP"
+.f.t delete 9.8  9.15
+.f.t insert 9.8 " 1.4600"
 }
 
 eval destroy [winfo child .]
