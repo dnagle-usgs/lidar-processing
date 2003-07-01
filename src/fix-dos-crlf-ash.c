4c447f9e498e50a95717cd85c5622b277651e744
Wayne Wright <wwright@usgs.gov>
2003-07-01 02:55:03 +0000

    Added small "C" program which repairs Ashtech gps data downloaded with the Windows/dos commandline ftp client without the "binary" option enabled.  The Windows/dos commandline ftp client "assumes" that data is ASCII unless specifically told it's binary with the binary flag.  The problem is that when thinks its downloading ASCII data, it converts all bytes with a value of 10 to a pair of bytes valued 13 and 10 respectively. This seriously fouls up binary data.  This program attempts to remove byte paris of 13,10 and replaced them with a single 10 value. Since this problem is very easy to have happen whenever a Windows computer is involved in the transfer of gps data from our ground stations to data processing systems, I've included it here.

---
+++ src/fix-dos-crlf-ash.c
@@ -0,0 +1,41 @@
+/**********************************************************
+
+   $Id$ 
+   W. Wright 6/30/2003
+
+   This program locates and removes linefeed characters
+inserted by the Windows commandline ftp program when it
+is used to download binary data without the "bi" option
+turned on.  Without the "bi" option, the dos ftp program 
+assumes the file is ASCII and it replaces all 0x0a 
+bytes with the byte pair 0x0d followed by 0x0a which is 
+a carriage return followed by a linefeed.  Needless to say,
+this seriously fouls up a binary file which contains any
+0x0a bytes.  
+
+The main use for this program is to "fix" raw binary 
+Ashtech data which was accidently downloaded without the
+"bi" option enabled.
+
+**********************************************************/
+#include <stdio.h>
+unsigned char c,d,e;
+unsigned int repairs = 0;
+main() {
+
+  while ( !feof(stdin) ) {		// do the following until end-of-file
+    c = getchar() & 0xff ;		// get a single char
+    if ( c == 0x0d ) {                  // see if it's a CR
+       d = getchar();                   // if it's a CR, get the next character
+       if ( d == 0x0a ) {               // If this one is a LF, then
+          putchar(d);                   //   send it to the output stream
+          if ( (++repairs % 1024) == 0 ) fprintf(stderr,"\r  %7d  repairs made", repairs );
+       } else {                           // if it wasn't a CRLF pair, then 
+          ungetc(d, stdin);             //   put "d" back on the input stream
+          putchar(c);                   // and output the previous "c"
+       }
+    } else putchar(c);                  // if "c" wasn't a CR, then simply output the char.
+ }
+ fprintf(stderr,"\r  %7d  repairs made", repairs );
+ fprintf(stderr,"\n");
+}
