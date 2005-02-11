240552f5b4583f4169f276480f2caf17437ddafe
Richard W. Mitchell <rwmitchell@usgs.gov>
2005-02-11 15:56:21 +0000

    Removed -w so we don't see all those perl warnings

--- src/shapefile_filter.pl
+++ src/shapefile_filter.pl
@@ -1,22 +1,13 @@
-#!/bin/sh -- # comment mentioning perl to avoid looping
-eval 'perl_check="`dirname $0`/`uname -p`/perl"; \
-  PATH="`dirname $0`:$PATH"; \
-    if [ -x /usr/local/bin/perl ]; then \
-      PATH="`dirname $perl_check`:$PATH"; export PATH; \
-      exec /usr/local/bin/perl -S $0 ${1+"$@"}; \
-    else \
-      exec /usr/bin/perl -S $0 ${1+"$@"}; \
-    fi'
-  if 0;
+#!/usr/bin/perl
 
-# Everything above here could be replaced with:
-#!/usr/bin/perl -w
+$Id    = '$Id$';
+$Source = '$Source$';
 
 sub showusage {
   print <<EOF;
 
-# $Id$
-# $Source$
+# $Id
+# $Source
 #
 
 $0 : file1.txt [file2.txt filen.txt]
