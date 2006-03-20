987b6dc2fb8c7c4b9daacca5aa7c41a8b399bfa5
Richard W. Mitchell <rwmitchell@usgs.gov>
2006-03-20 21:02:30 +0000

    Changed to process gps .cfg files

--- src/get_location.pl
+++ src/get_location.pl
@@ -13,6 +13,11 @@ sub showusage {
 $Id
 Extract the antenna location from the -ins.txt files
 
+$0 [-gps] [-kml] File1 File2 FileN
+
+-gps:  process a gps .cfg file, default is to process an -ins.txt file
+-kml:  create a basestations.kml file for google earth
+
 EOF
   exit(0);
 }
@@ -22,7 +27,8 @@ sub get_cli_opts {
   &showusage unless
   &NGetOpt(
   "help",     # help
-  "kmz",      # output a doc.kml file
+  "gps",      # use gps.cfg file for input instead of ins.txt
+  "kml",      # output a basestations.kml file
   "myint=i",
   "myfloat=f",
   );
@@ -36,7 +42,7 @@ sub create_head {
 <?xml version="1.0" encoding="UTF-8"?>
 <kml xmlns="http://earth.google.com/kml/2.0">
 <Document>
-  <name>BaseStations.kmz</name>
+  <name>BaseStations.kml</name>
   <Folder>
     <name>BaseStations</name>
     <open>1</open>
@@ -69,15 +75,20 @@ HERE_TARGET
 printf("myint   = %d\n", $opt_myint  ) if ( $opt_myint  );
 printf("myfloat = %f\n", $opt_myfloat) if ( $opt_myfloat);
 
-if ( $opt_kmz ) {
+if ( $opt_kml ) {
   create_head();
   create_tail();
   open( KMZ, ">basestations.kml");
   print KMZ $HEAD;
 }
 
+if ( $opt_gps ) {  # create a temporary file to hold the data
+  $tmpfile = "/tmp/get_location.$$";
+  open( GPS, ">$tmpfile") || die( "Unable to open $tmpfile for writing\n");
+}
+
 for ( $argc; $argc <= $#ARGV; ++$argc ) {
-  open(TXT, $ARGV[$argc]) || die("Unable to open $ARGV[$argc]\n");
+  open( TXT, $ARGV[$argc]) || die("Unable to open $ARGV[$argc]\n");
 
   $done = 0;
 
@@ -86,55 +97,104 @@ for ( $argc; $argc <= $#ARGV; ++$argc ) {
     chop $line;    #           remove \n
     # printf("->%s\n", $line);
 
-    ($flag, $data) = split(/:/, $line);
+    if ( ! $opt_gps ) {                                 # process an -ins.txt file
+      ($flag, $data) = split(/:/, $line);
 
-    if ( $flag =~ "Project" ) {
-      $proj = $data;
-      $proj =~ s/^ *//;
-    }
+      if ( $flag =~ "Project" ) {
+        $proj = $data;
+        $proj =~ s/^ *//;
+      }
 
-    if ( $flag =~ "Master" ) {
-      $ant1 = $data;
-      $ant2 = <TXT>;
-      chop $ant2;  # dos file, remove \r
-      chop $ant2;  #           remove \n
+      if ( $flag =~ "Master" ) {
+        $ant1 = $data;
+        $ant2 = <TXT>;
 
-      $ant1 =~ s/^ *//;
-      $ant2 =~ s/^ *//;
+        chop $ant2;  # dos file, remove \r
+        chop $ant2;  #           remove \n
+        
+        $ant1 =~ s/^ *//;
+        $ant2 =~ s/^ *//;
 
-      ($namestr, $hghtstr, $status)  = split(",", $ant1);
-      ($junk, $name) = split(" ", $namestr);
-      ($junk, $hght) = split(" ", $hghtstr);
+        ($namestr, $hghtstr, $status)  = split(",", $ant1);
+        ($junk, $name) = split(" ", $namestr);
+        ($junk, $hght) = split(" ", $hghtstr);
 
-      ($junk,
-       $latdeg, $latmin, $latsec,
-       $londeg, $lonmin, $lonsec,
-       $ehght,
-       $junk2 ) = split(/[ ,]+/, $ant2);
+        ($junk,
+         $latdeg, $latmin, $latsec,
+         $londeg, $lonmin, $lonsec,
+         $ehght,
+         $junk2 ) = split(/[ ,]+/, $ant2);
 
-       $lat = abs($latdeg) + ($latmin/60.0) + ($latsec/3600.0);
-       $lon = abs($londeg) + ($lonmin/60.0) + ($lonsec/3600.0);
+         $lat = abs($latdeg) + ($latmin/60.0) + ($latsec/3600.0);
+         $lon = abs($londeg) + ($lonmin/60.0) + ($lonsec/3600.0);
 
-       $lat *= -1 if ( $latdeg < 0);
-       $lon *= -1 if ( $londeg < 0);  # recover the lost sign bit
+         $lat *= -1 if ( $latdeg < 0);
+         $lon *= -1 if ( $londeg < 0);  # recover the lost sign bit
 
-      # printf("%s\n%s\n%s\n", $proj, $ant1, $ant2);
-      # printf("<%s>\n", $proj );
-      # printf("<%s>\n", $name );
-      printf("%s,%s,", $proj, $name);             # do we need ant height?
-      printf("%f,%f,%s", $lat, $lon, $ehght);
-      # printf("%s %s %s\n", $londeg, $lonmin, $lonsec);
-      printf("\n");
-      if ( $opt_kmz ) {
-        create_placemark();
-        print KMZ $PLACE;
+        # printf("%s\n%s\n%s\n", $proj, $ant1, $ant2);
+        # printf("<%s>\n", $proj );
+        # printf("<%s>\n", $name );
+        printf("%s,%s,", $proj, $name);             # do we need ant height?
+        printf("%f,%f,%s", $lat, $lon, $ehght);
+        # printf("%s %s %s\n", $londeg, $lonmin, $lonsec);
+        printf("\n");
+        if ( $opt_kml ) {
+          create_placemark();
+          print KMZ $PLACE;
+        }
+      }
+      $done = 1 if ( $flag =~ "Remote" );
+    } else {                                            # process a gps.cfg file
+      ($flag, $data) = split(/  = /, $line);
+      $name   = $data if ( $flag =~ "MASTER_NAME" );
+      $posstr = $data if ( $flag =~ "MASTER_POS"  );
+      $antstr = $data if ( $flag =~ "MASTER_ANT"  );
+
+      if ( $flag =~ "MASTER_ANT" ) {                  # the last line to get, process everything
+        ($latdeg, $latmin, $latsec,
+         $londeg, $lonmin, $lonsec,) = split(/[ ,]+/, $posstr);
+
+         $lat = abs($latdeg) + ($latmin/60.0) + ($latsec/3600.0);
+         $lon = abs($londeg) + ($lonmin/60.0) + ($lonsec/3600.0);
+
+         $lat *= -1 if ( $latdeg < 0);
+         $lon *= -1 if ( $londeg < 0);  # recover the lost sign bit
+
+        printf GPS ("%s,%f,%f\n", $name, $lat, $lon);
       }
     }
 
-    $done = 1 if ( $flag =~ "Remote" );
   }
+
+  
   close(TXT);
 
 }
-print  KMZ $TAIL      if ( $opt_kmz );
-close( KMZ )          if ( $opt_kmz );
+
+if ( $opt_gps ) {    # close the gps file, filter the data, then output the results
+  close(GPS);
+
+  # we'll trust sort and uniq are safe, sort is in /bin for linux and /usr/bin for os x.
+  open( GPS, "sort $tmpfile | uniq |") || die ("Unable to open pipen");
+  $ehght = 0.0;   # don't have this value
+  $cnt = 0;
+  while ( $line = <GPS> ) {
+    chop $line;    #           remove \n
+    ($name, $lat, $lon) = split(/,/, $line);
+
+    $name = "pos$cnt" if ( $name eq "" );
+    ++$cnt;
+
+    printf ("%s |  %f |  %f\n", $name, $lat, $lon);
+    if ( $opt_kml ) {
+      create_placemark();
+      print KMZ $PLACE;
+    }
+  }
+  close(GPS);
+  unlink($tmpfile);
+
+}
+
+print  KMZ $TAIL      if ( $opt_kml );
+close( KMZ )          if ( $opt_kml );
