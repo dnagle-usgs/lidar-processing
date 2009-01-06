421009ea0d0625ad037def7e74091871bf78e5ed
Richard Mitchell <rwmitchell@usgs.gov>
2009-01-06 21:57:44 +0000

    Server mode now automatically checks every 10 seconds if there are new files to be processed in /tmp/batch/jobs (as placed there by mbatch_process() in ALPS) and then sends them to all connected clients that are "ready".  This removes the need to manually tell each client to start requesting files.
    A client will automatically request a new file after completing a file.
    This method keeps the turn-around time to a minimum when there are files
    in the queue.
    
    Files now go through 4 subdirectories under /tmp/batch before being removed.
    
    jobs: files are created and placed here by mbatch_process
    farm: the batcher.tcl server moves the file to be processed here before
          telling the client to use it.  this removes the conflict that
          occurred when a second client would request a file before the
          first client had moved it to work.
    work: the client moves the file to work when processing is started
    done: the client moves the file to done when processing is completed.
    
    mbatch_process() (using show_tiles.pl) looks in the done directory to
    see which files have been completed, updates the map display, and then
    removes the files.
    
    To run the batch server:
      ./batcher.tcl server
    
    To run a client (up to 1 per cpu or core available):
      ./batcher.tcl localhost
    
    By running two clients, processing time for batch_process can be cut in half.

--- src/batcher.tcl
+++ src/batcher.tcl
@@ -4,10 +4,13 @@ exec tclsh "$0" ${1+"$@"}
 # exec /opt/eaarl/bin/tclsh "$0" ${1+"$@"}
 # vim: set tabstop=3 softtabstop=3 shiftwidth=3 autoindent shiftround expandtab:
 
+package require Tclx
+
 # set host localhost
 set host [lindex $argv 0]
 set port 9900
 set jdir /tmp/batch/jobs
+set fdir /tmp/batch/farm
 set wdir /tmp/batch/work
 set ddir /tmp/batch/done
 
@@ -15,47 +18,68 @@ set ddir /tmp/batch/done
 
 #-----------------------------------------------------
 
+proc get_file { sock addr } {
+   global jdir fdir 
+
+   catch { exec ls $jdir } res
+   # puts [llength $res]
+   set fn [ lindex $res 0 ]
+   if { [ llength $res] > 0 } {
+      catch { exec mv $jdir/$fn $fdir } res
+      puts "sending file: ($fdir) $fn"
+      puts $sock "file: $fn"
+      alarm 3
+   } else {
+      puts "DONE: $addr $sock"
+      puts $sock "done:"
+   }
+}
 
 proc Service { sock addr } {
-   global jdir echo connections
+   global fdir jdir echo status
 
-   puts "Service: $sock $addr"
+   # puts "Service: $sock $addr"
    if { [eof $sock] || [ catch { gets $sock line } ]} {
       close $sock
-      puts "Close $echo(addr,$sock)"
-      unset echo(addr,$sock)
+      puts "Close echo($addr,$sock)"
+      unset echo($addr,$sock)
    } else {
-      puts "Open $echo(addr,$sock)"
-      puts "$addr: $line"
+      # puts "Open echo($addr,$sock)"
+      puts "RCVD: $addr $sock: $line"
       # puts "got $line"
-      set lst  [ split $line " " ];
-      set cmd  [ lindex $lst 0 ];
-      set args [ lrange $lst 1 end ];
+      set lst  [ split $line " " ]
+      set cmd  [ lindex $lst 0 ]
+      set args [ lrange $lst 1 end ]
 
       # Process command from client
       switch $cmd {
          list {
-            catch { exec ls $jdir } res;
-            puts [llength $res];
-            set fn [ lindex $res 0 ];
-            if { [ llength $res] > 0 } {
-               puts $sock "file: $fn";
-            } else {
-               puts $sock "done:";
-            }
+            get_file $sock $addr
+         }
+
+         quit {
+            puts "got close request on $sock"
+            close $sock
+            puts "Close echo($addr,$sock)"
+            unset echo($addr,$sock)
+         }
+
+         status {
+            puts "Received status from $addr $sock: $args\n"
+            set status($sock) $args
          }
 
          default {
             if { [llength $lst] == 1 } {
                puts "length = 1"
                catch { exec $cmd } res
-               puts $cmd->$res ;# local logging
+               puts $cmd->$res         # local logging
                puts $sock $cmd->$res
             } else {
                puts "length > 1"
                foreach arg $args {
-                  catch { exec $cmd $arg } res;
-                  puts $cmd->$res ;# local logging
+                  catch { exec $cmd $arg } res
+                  puts $cmd->$res      # local logging
                   puts $sock $cmd->$res
                }
             }
@@ -63,21 +87,50 @@ proc Service { sock addr } {
       }
    }
 }
+
+#------------------------------------------------------
+proc check_for_files { } {
+   global echo sock status
+
+   set myt [ clock format [clock seconds] -format "%H:%M:%S"]
+   puts -nonewline "\n$myt: Timer expired: "
+   alarm 10 ; # reset the clock
+
+   # puts "echo :  [lindex $echo 0]"
+   if  { [array exists echo] } {
+      set sz [array size echo ];
+      puts "We have $sz connections!"
+      set elem [ array startsearch echo ]
+      for { set i 1 } { $i <= $sz } { incr i } {
+         # puts "Search : $elem"
+         set child [ array nextelement echo $elem ]
+         # puts "child :  $child"
+         set lst [ split $child ","]
+         set addr [ lindex $lst 0 ]
+         set sock [ lindex $lst 1 ]
+         puts "ADDR: $addr $sock -> $status($sock)"
+         if { $status($sock) eq "ready" } {
+            get_file $sock $addr
+         }
+      }
+   }
+
+}
 #------------------------------------------------------
 
-
 proc server {sock addr port} {
-   global jdir echo connections
+   global jdir echo 
 
    # Record client's information
 
-   puts "Accept connection $sock from $addr on port $port";
-   set echo(addr,$sock) [ list $addr $port ]
-   # puts "Open $echo(addr, $sock)"
+   puts "Accept connection $sock from $addr on port $port"
+   set echo($addr,$sock) [ list $addr $port ]
+   puts "Open echo($addr, $sock)"
+   puts $sock "Open echo($addr, $sock)"
 
    fconfigure $sock -buffering line
 
-   # set connections($sock) $sock
+   # set connections($sock) sock
 
    # Set up a callback for when client sends data
 
@@ -89,48 +142,66 @@ proc open_server { port } {
    set s [ socket -server server $port ]
    vwait forever
 }
+
 #-----------------------------------------------------
 
 puts "ARGV:  [lindex $argv 0]"
 # invoke as: $0 server
 
 if { $host eq "server"} {
+   catch { exec mkdir -p $jdir }
+   catch { exec mkdir -p $fdir }
+   catch { exec mkdir -p $wdir }
+   catch { exec mkdir -p $ddir }
+   signal trap  [ list ALRM ] check_for_files
+   alarm 5
    open_server $port
-} else {
+} else {    # start as client
    # set sock [socket $host $port]
    set sock [socket $host $port]
    fconfigure $sock -buffering line
    fileevent $sock readable [list client'read $sock]
    fileevent stdin readable [list client'send $sock]
+   puts $sock "status ready"
 }
-#-----------------------------------------------------
+
+#---------------- CLIENT Code-------------------------
+
 proc client'read sock {
-   global jdir wdir ddir doall
+   global jdir wdir fdir ddir doall
    if {[eof $sock]} {close $sock; exit}
    gets $sock line
-   set lst [ split $line " "];
-   set cmd [ lindex $lst 0 ];
-   set args [ lrange $lst 1 end ];
+   set lst [ split $line " "]
+   set cmd [ lindex $lst 0 ]
+   set args [ lrange $lst 1 end ]
    switch $cmd {
       file: {
-         puts "line: $line"
-         puts "args: $args";
-         catch { exec mv $jdir/$args $wdir } res;
+         puts "line(file): $line"
+         puts "args: $args"
+         catch { exec mv $fdir/$args $wdir } res
          if { $res > "" } {
-            puts "res : $res";
+            puts "res : $res"
          }
+         set doall list
+         puts $sock "status $args"
          catch { exec /opt/eaarl/lidar-processing/src/cmdline_test $wdir/$args } res
          if { $res > "" } {
-            puts "res : $res";
+            puts "res : $res"
          }
-         catch { exec mv $wdir/$args $ddir } res;
+         catch { exec mv $wdir/$args $ddir } res
          if { $doall > "" } {
             puts $sock $doall
          }
+         puts $sock "status ready"
+         puts "Completed $args\n"
+      }
+
+      done: {
+         set doall ""
+         puts "line(done): $line"
       }
       default {
-         set doall "";
-         puts "line: $line"
+         puts "line(default): $line."
       }
    }
 }
@@ -141,11 +212,11 @@ proc client'send sock {
 
    switch $line {
        exit {
-         exit 0;
+         exit 0
       }
       work {
-         catch { exec ls $wdir } res;
-         puts "work: $res";
+         catch { exec ls $wdir } res
+         puts "work: $res"
       }
       run {
          set doall list
