2fa51346617e54492f9be4bf9ed6a14f7bb66c82
David B. Nagle <dnagle@usgs.gov>
2012-09-13 10:00:29 -0400

    No longer need atm/load.tcl.

--- src/plugins/atm/load.tcl
+++
@@ -1,6 +0,0 @@
-.l1wid.mb.plugins.atm delete 0 end
-.l1wid.mb.plugins.atm add command -label "Plugin has been loaded" \
-        -command [list tk_messageBox \
-                -icon info \
-                -type ok \
-                -message "This plugin is already loaded"]
