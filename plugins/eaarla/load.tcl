0a6f105071da891e699389234fc65c62925b87c8
David B. Nagle <dnagle@usgs.gov>
2018-03-22 17:59:50 -0400

    Changes to allow merging from previous post-subrepository-merge repository.

--- plugins/eaarla/load.tcl
+++
@@ -1,8 +0,0 @@
-package require plugins
-
-::tcl::tm::path add [file join [app_root_dir] .. plugins eaarla tcl]
-
-package require mission::eaarl
-package require eaarl
-package require sf::model::cir
-package require sf::model::rgb
